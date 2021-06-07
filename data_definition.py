a43024101b0894a383d8637d0a2fafb05f899b32
malleshwarisp <malleshwari135@gmail.com>
2021-06-07 10:00:07 +0530

    removed users table

--- data_definition.py
+++ data_definition.py
@@ -16,21 +16,8 @@ def create_tasks_table():
     db_con.commit()
 
 
-def create_users_table():
-    cur = db_con.cursor()
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username VARCHAR(30),
-        password_hash VARCHAR(66),
-        permission INTEGER
-    )"""
-    )
-    db_con.commit()
-
 
 def create_all_tables():
-    create_users_table()
     create_tasks_table()
     close_db()
 
